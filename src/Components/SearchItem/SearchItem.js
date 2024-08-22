21ddaf8c571b11d3f6a1727fb8462a51311bafff
Ayush Sharma <ayush22@navgurukul.org>
2024-08-22 16:18:03 +0530

    Add More Details

---
+++ src/Components/SearchItem/SearchItem.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+// import './style.css'
+
+const SearchItem = () => {
+  const [Data, setData] = useState([]);
+  const [Menu, setMenu] = useState([]);
+  const [input, setInput] = useState([]);
+  const [find, setFind] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s"
+      );
+      const result = await response.json();
+      console.log(result);
+      setData(result);
+
+
+    };
+  });
+};
+export default SearchItem;
