5c8259d61d6f230a645aec1f4941cb17492ca6ed
Ayush Sharma <ayush22@navgurukul.org>
2024-08-22 16:18:51 +0530

    Import

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import Invoice from "./Components/Invoice";
-import SearchItem from "./Components/SearchItem";
-import Home from "./Components/Home";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import SearchItem from "./Components/SearchItem/SearchItem";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-    <SearchItem/>
-    <Home/>
+      <Navbar/>
+      <SearchItem />
     </>
-  )
-} 
+  );
+}
 
 export default App;
-
