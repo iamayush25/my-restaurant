8e6b4f770fb0e9ce8a4684a60a22516461334b02
Ayush Sharma <ayush22@navgurukul.org>
2024-08-23 18:50:26 +0530

    Import New Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Home from "./Components/Home/Home";
-import SearchItem from "./Components/SearchItem/SearchItem";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <SearchItem />
+      <Navbar />
     </>
   );
 }
